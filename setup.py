116d8dc81e44264d316ffc34e1093c31d789d408
Daniel Ward <d@d-w.me>
2015-01-10 16:57:56 +0000

    Version update fix for 0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [str(ir.req) for ir in install_requirements]
 
 setup(
     name='python-mibody',
-    version='0.1',
+    version='0.1.2',
     description=(
         'Lightweight open source Python 3 package for reading data from '
         'Salter MiBody scales'),
@@ -22,7 +22,7 @@ setup(
     author_email='d@d-w.me',
     packages=['mibody'],
     url='https://github.com/danielward/python-mibody',
-    download_url='https://github.com/danielward/python-mibody/tarball/0.1',
+    download_url='https://github.com/danielward/python-mibody/tarball/0.1.2',
     install_requires=requirements,
     test_suite='tests',
 )
